9038e100e82dc53dda740ae24ac5dd841c1632e3
wangbo <1316065221@qq.com>
2020-03-29 21:22:08 +0800

    file change

---
+++ eureka_client/src/main/java/com/spring/cloud/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.spring.cloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 汪波
+ * @Date 21:12 2020/3/29
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
